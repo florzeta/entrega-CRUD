0bb8fc7b34b5197bb0b0fb4cd29211a5384a4d03
florzeta <99660646+florzeta@users.noreply.github.com>
2024-05-07 21:02:02 -0300

    commit

--- blog-app/backend/routes/admin/publicaciones.js
+++ blog-app/backend/routes/admin/publicaciones.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var publicacionesModel = require('./../../models/publicacionesModel');
+var publicacionesModel = require('../../models/publicacionesModel');
 const util = require('util');
 const cloudinary = require('cloudinary').v2;
 const uploader = util.promisify(cloudinary.uploader.upload);
@@ -11,12 +11,12 @@ router.get('/', async function (req, res, next) {
     publicaciones = publicaciones.map(publicacion => {
         if (publicacion.img_id) {
             const imagen = cloudinary.image(publicacion.img_id, {
-                width: 100,
-                height: 100,
+                width: 70,
+                height: 70,
                 crop: 'fill'
             });
             return {
-                ...publicacion, 
+                ...publicacion,
                 imagen
             }
         } else {
@@ -27,18 +27,17 @@ router.get('/', async function (req, res, next) {
         }
     });
 
-res.render('admin/publicaciones', {
+    res.render('admin/publicaciones', {
         layout: 'admin/layout',
         usuario: req.session.nombre,
         publicaciones
     });
 });
 
-
 router.get('/agregar', (req, res, next) => {
     res.render('admin/agregar', {
         layout: 'admin/layout'
-    });
+    })
 });
 
 router.post('/agregar', async (req, res, next) => {
@@ -73,6 +72,7 @@ router.post('/agregar', async (req, res, next) => {
     }
 });
 
+
 router.get('/eliminar/:id', async (req, res, next) => {
     var id = req.params.id;
     await publicacionesModel.deletePublicacionById(id);
@@ -90,11 +90,27 @@ router.get('/modificar/:id', async (req, res, next) => {
 
 router.post('/modificar', async (req, res, next) => {
     try {
-        let obj = {
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+        if (req.body.img_delete === "1") {
+            img_id = null;
+            borrar_img_vieja = true;
+        } else {
+            if (req.files && Object.keys(req.files).length > 0) {
+                imagen = req.files.imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja = true;
+            }
+        }
+        if (borrar_img_vieja && req.body.img_original) {
+            await (destroy(req.img_original));
+        }
+
+        var obj = {
             titulo: req.body.titulo,
             subtitulo: req.body.subtitulo,
-            // img_id: req.body.imagen,
-            cuerpo: req.body.cuerpo
+            cuerpo: req.body.cuerpo,
+            img_id
         }
 
         await publicacionesModel.modificarPublicacionById(obj, req.body.id);
