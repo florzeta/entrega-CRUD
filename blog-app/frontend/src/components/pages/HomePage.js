09788e2ca108d2394b80c1fe98387d308f7f21c7
florzeta <99660646+florzeta@users.noreply.github.com>
2024-05-11 18:41:41 -0300

    home- arreglos

--- blog-app/frontend/src/components/pages/HomePage.js
+++ blog-app/frontend/src/components/pages/HomePage.js
@@ -39,7 +39,7 @@ const HomePage = () => {
                         <p>Sumérgete en un mundo de historias, consejos y descubrimientos. Desde consejos de estilo de vida hasta exploraciones en profundidad de temas fascinantes, encontrarás inspiración y conocimiento aquí. ¡Explora, aprende y comparte tus pensamientos con nosotros!</p>
                     </div>
                     <section className="featured-posts">
-                        <h2>Destacado del día:</h2>
+                        <h2>Destacados del día:</h2>
                         {publicacionesDestacadas.length > 0 && (
                         <Card className="featured-card">
                         <Card.Body>
